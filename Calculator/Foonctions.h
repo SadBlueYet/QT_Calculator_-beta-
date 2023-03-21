f631e2d4f9a08699346985da2133d81847303403
SadBlueYet <88283979+SadBlueYet@users.noreply.github.com>
2023-03-22 03:30:32 +0700

    Add files via upload

---
+++ Calculator/Foonctions.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <stack>
+#include <sstream>
+#include <math.h>
+#include <string>
+class Stack;
+double exitString(std::string bufptr);
+double mathOperations(std::string a);
+void trigonometry(double value, int& count, std::string& exitString);
+void createValue(std::string& value, char ch);
+std::string valueProcessing(double value);
+
